8233dadbe9faebbe1868c436ad90244f470c4914
Hunter Wallen <hunter.wallen@projectcanary.com>
2022-10-10 15:44:07 -0600

    flag mutable

--- android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
+++ android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
@@ -560,7 +560,7 @@ public class RNSerialportModule extends ReactContextBaseJavaModule {
   private void requestUserPermission() {
     if(device == null)
       return;
-    PendingIntent mPendingIntent = PendingIntent.getBroadcast(reactContext, 0 , new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT);
+    PendingIntent mPendingIntent = PendingIntent.getBroadcast(reactContext, 0 , new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE);
     usbManager.requestPermission(device, mPendingIntent);
   }
 
